d9b837af17bd736ef0cf076f5de6ee6ac3b55050
Konock <bkonock@gmail.com>
2019-01-10 14:13:55 +0100

    create models for deck

---
+++ src/main/java/com/greenfoxacademy/dah/models/Deck.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy.dah.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Deck {
+
+    private List<BlackCard> blackCards;
+    private List<WhiteCard> whiteCards;
+}
