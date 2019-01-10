fa65db4516a015c3dddf418585e72aa713c329cb
Konock <bkonock@gmail.com>
2019-01-10 13:57:52 +0100

    initialize project

---
+++ src/main/java/com/greenfoxacademy/dah/DahApplication.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.dah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DahApplication.class, args);
+    }
+
+}
+
