cd4c14c272c749e30a33143430d2f21e60d02ed0
takacscsaba <takacs.csaba@hotmail.com>
2019-01-11 12:21:04 +0100

    fix(imports): don't have to import each and every thing every time

--- src/main/java/com/greenfoxacademy/dah/controllers/LobbyController.java
+++ src/main/java/com/greenfoxacademy/dah/controllers/LobbyController.java
@@ -1,6 +1,5 @@
 package com.greenfoxacademy.dah.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.greenfoxacademy.dah.models.Lobby;
@@ -10,7 +9,11 @@ import com.greenfoxacademy.dah.services.LobbyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/cah/game")
